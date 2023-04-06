960db625b070cba1254443d1c4f90ddf91a9f1d5
Omkar Patil <>
2023-04-06 14:28:49 +0000

    update

--- main.py
+++ main.py
@@ -7,10 +7,37 @@ app = Flask(
   static_folder='static'
 )
 
+JOBS = [
+  {
+    'id' : 1,
+    'Role' : 'Andriod Devloper',
+    'Location' : 'Mumbai',
+    'Salary' : 'RS. 10,00,000'
+  },
+  {
+    'id' : 2,
+    'Role' : 'Frontend Devloper',
+    'Location' : 'Bengluru',
+    'Salary' : 'RS. 15,00,000'
+  },
+  {
+    'id' : 3,
+    'Role' : 'Backend Devloper',
+    'Location' : 'Remote',
+    # 'Salary' : 'RS. 20,00,000'
+  },
+  {
+    'id' : 4,
+    'Role' : 'Data Analyst',
+    'Location' : 'San Fansisco',
+    'Salary' : '$. 100,000'
+  }
+]
+
 # Index page (now using the index.html file)
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', jobs=JOBS, conpany_name= 'Bright')
 
 if __name__ == '__main__':
   # Run the Flask app
